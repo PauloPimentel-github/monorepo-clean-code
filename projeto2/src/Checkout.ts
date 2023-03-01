aab3a68f561dace132584604f57753a0618e291a
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-02-28 21:22:58 -0300

    Test Patterns - Stub, Spy, Mock e Fake

--- projeto2/src/Checkout.ts
+++ projeto2/src/Checkout.ts
@@ -1,9 +1,20 @@
+import CouponRepository from "./CouponRepository";
 import CouponRepositoryDatabase from "./CouponRepositoryDatabase";
+import CurrencyGateway from "./CurrencyGateway";
 import CurrencyGatewayHttp from "./CurrencyGatewayHttp";
+import FreightCalculator from "./FreightCalculator";
+import ProductRepository from "./ProductRepository";
 import ProductRepositoryDatabase from "./ProductRepositoryDatabase";
 import { validate } from "./version2/validator";
 
 export default class Checkout {
+
+    constructor(
+        readonly currencyGateway: CurrencyGateway = new CurrencyGatewayHttp(),
+        readonly productRepository: ProductRepository = new ProductRepositoryDatabase(),
+        readonly couponRepository: CouponRepository = new CouponRepositoryDatabase()
+        ) {
+    }
 
     async execute(input: Input): Promise<Output> {
         const isValid = validate(input.cpf);
@@ -12,15 +23,13 @@ export default class Checkout {
             total: 0,
             freight: 0
         };
-        const currencyGateway = new CurrencyGatewayHttp();
-        const currencies = await currencyGateway.getCurrencies();
+        const currencies = await this.currencyGateway.getCurrencies();
         const items: number[] = [];
         if (input.items) {
             for (const item of input.items) {
                 if (item.quantity <= 0) throw new Error("Invalid quantity");
                 if (items.includes(item.idProduct)) throw new Error("Duplicated item");
-                const productRepository = new ProductRepositoryDatabase();
-                const productData = await productRepository.getProduct(item.idProduct);
+                const productData = await this.productRepository.getProduct(item.idProduct);
                 if (productData.width <= 0 || productData.height <= 0 || productData.length <= 0 || parseFloat(productData.weight) <= 0) throw new Error("Invalid dimension")
                 if (productData.currency === "USD") {
                     output.total += parseFloat(productData.price) * item.quantity * currencies.usd;
@@ -28,16 +37,16 @@ export default class Checkout {
                     output.total += parseFloat(productData.price) * item.quantity;
                 }
                 
-                const volume = productData.width / 100 * productData.height / 100 * productData.length / 100;
-                const density = parseFloat(productData.weight) / volume;
-                const itemFreight = 1000 * volume * (density / 100);
+                // const volume = productData.width / 100 * productData.height / 100 * productData.length / 100;
+                // const density = parseFloat(productData.weight) / volume;
+                // const itemFreight = 1000 * volume * (density / 100);
+                const itemFreight = FreightCalculator.calculate(productData);
                 output.freight += Math.max(itemFreight, 10) * item.quantity;
                 items.push(item.idProduct);
             }    
         }
         if (input.coupon) {
-            const couponRepository = new CouponRepositoryDatabase();
-            const couponData = await couponRepository.getCoupon(input.coupon);
+            const couponData = await this.couponRepository.getCoupon(input.coupon);
             if (couponData.expire_date.getTime() > new Date().getTime()) {
                 const percentage = parseFloat(couponData.percentage);
                 output.total -= (output.total * percentage) / 100;
--- projeto2/test/Checkout.test.ts
+++ projeto2/test/Checkout.test.ts
@@ -1,4 +1,10 @@
 import Checkout from "../src/Checkout";
+import sinon from "sinon";
+import CurrencyGatewayHttp from "../src/CurrencyGatewayHttp";
+import ProductRepositoryDatabase from "../src/ProductRepositoryDatabase";
+import CouponRepositoryDatabase from "../src/CouponRepositoryDatabase";
+import CurrencyGateway from "../src/CurrencyGateway";
+import ProductRepository from "../src/ProductRepository";
 
 let checkout: Checkout;
 
@@ -123,11 +129,83 @@ test("Deve criar um pedido com 1 produto calculando o frete valor mínimo", asyn
     expect(output.total).toBe(40);
 });
 
-test("Deve criar um pedido com 1 produto em dólar", async () => {
+test("Deve criar um pedido com 1 produto em dólar usando um stub", async () => {
+    const stubCurrencyGateway = sinon.stub(CurrencyGatewayHttp.prototype, "getCurrencies").resolves({
+        usd: 3
+    });
+    const stubProductRepository = sinon.stub(ProductRepositoryDatabase.prototype, "getProduct").resolves(
+        { idProduct: 5, description: "A", price: 1000, width: 10, height: 10, length: 10, weight: 10, currency: "USD" }
+    );
     const input = {
         cpf: "407.302.170-27",
         items: [
             { idProduct: 5, quantity: 1 }
+        ]
+    }
+    const output = await checkout.execute(input);
+    expect(output.total).toBe(3000);
+    stubCurrencyGateway.restore();
+    stubProductRepository.restore();
+});
+
+test("Deve criar um pedido com 3 produtos com cupom de desconto com spy", async () => {
+    const spyProductRepository = sinon.spy(ProductRepositoryDatabase.prototype, "getProduct");
+    const spyCouponRepository = sinon.spy(CouponRepositoryDatabase.prototype, "getCoupon");
+    const input = {
+        cpf: "407.302.170-27",
+        items: [
+            { idProduct: 1, quantity: 1 },
+            { idProduct: 2, quantity: 1 },
+            { idProduct: 3, quantity: 3 }
+        ],
+        coupon: "VALE20"
+    }
+    const output = await checkout.execute(input);
+    expect(output.total).toBe(6090);
+    expect(spyCouponRepository.calledOnce).toBeTruthy();
+    expect(spyCouponRepository.calledWith("VALE20")).toBeTruthy();
+    expect(spyProductRepository.calledThrice).toBeTruthy();
+    spyCouponRepository.restore();
+    spyProductRepository.restore();
+});
+
+test("Deve criar um pedido com 1 produto em dólar usando um mock", async () => {
+    const mockCurrencyGateway = sinon.mock(CurrencyGatewayHttp.prototype);
+    mockCurrencyGateway.expects("getCurrencies").once().resolves({
+        usd: 3
+    });
+    const input = {
+        cpf: "407.302.170-27",
+        items: [
+            { idProduct: 5, quantity: 1 }
+        ]
+    }
+    const output = await checkout.execute(input);
+    expect(output.total).toBe(3000);
+    mockCurrencyGateway.verify();
+    mockCurrencyGateway.restore();
+});
+
+test("Deve criar um pedido com 1 produto em dólar usando um fake", async () => {
+    const currencyGateway: CurrencyGateway = {
+        async getCurrencies(): Promise<any> {
+            return {
+                usd: 3
+            }
+        }
+    }
+    const productRepository: ProductRepository = {
+        async getProduct(idProduct: number): Promise<any> {
+            return { 
+                idProduct: 6, description: "A", price: 1000, width: 10, height: 10, length: 10, weight: 10, currency: "USD" 
+            }
+        }
+    }
+    checkout = new Checkout(currencyGateway, productRepository);
+    const input = {
+        cpf: "407.302.170-27",
+        items: [
+            { idProduct: 6, quantity: 1 }
         ]
     }
     const output = await checkout.execute(input);
