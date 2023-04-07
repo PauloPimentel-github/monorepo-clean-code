830df8c277c4793ae9cd27500a944cce001ca626
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-04-07 15:50:31 -0300

    Implementação do Projeto - Parte 5 (Domain-Driven Design - Context Bounded Freight)

--- projeto2/backend/checkout/src/application/usecase/SimulateFreight.ts
+++ projeto2/backend/checkout/src/application/usecase/SimulateFreight.ts
@@ -1,22 +1,29 @@
-import FreightCalculator from "../../domain/entity/FreightCalculator";
 import ProductRepository from "../../application/repository/ProductRepository";
-import ProductRepositoryDatabase from "../../infra/repository/ProductRepositoryDatabase";
+import FreightGatewayHttp from "../../infra/gateway/FreightGatewayHttp";
+import AxiosAdapater from "../../infra/http/AxiosAdapter";
+import FreightGateway, { Input as FreightInput } from "../gateway/FreightGateway";
 
 export default class SimulateFreight {
 
-    constructor(readonly productRepository: ProductRepository) {}
+    constructor(
+        readonly productRepository: ProductRepository,
+        readonly freightGateway: FreightGateway = new FreightGatewayHttp(new AxiosAdapater())
+    ) {
+    }
 
     async execute(input: Input): Promise<Output> {
         const output: Output = {
             freight: 0
         };
+        const freightInput: FreightInput = { items: [] };
         if (input.items) {
             for (const item of input.items) {
                 const product = await this.productRepository.getProduct(item.idProduct);
-                const itemFreight = FreightCalculator.calculate(product, item.quantity);
-                output.freight += Math.max(itemFreight, 10);
+                freightInput.items.push({ width: product.width, height: product.height, length: product.length, weight: product.weight, quantity: item.quantity });
             }    
         }
+        const freightOutput = await this.freightGateway.calculateFreight(freightInput);
+        output.freight = freightOutput.freight;
         return output;
     }
 }
